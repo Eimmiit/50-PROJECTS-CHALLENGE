8bab47614ecccc9859caee10b22739c320f6e0dc
Timilehin Orimisan <eiimmiit@gmail.com>
2026-04-01 16:26:49 -0700

    added little changes in47

--- 50. Random Img Generator/scripts.js
+++ 50. Random Img Generator/scripts.js
@@ -1,18 +1,18 @@
 const container = document.querySelector('.container')
-const unsplashURL = 'https://source.unsplash.com/random/'
+const unsplashURL = 'https://i.pravatar.cc/'
 const rows = 10
-
+// 'https://source.unsplash.com/random/'
 for (let i = 0; i < rows * 3; i++){
     const img = document.createElement('img')
-    img.src = `${unsplashURL}${getRandomSize}`
+    img.src = `${unsplashURL}${getRandomSize()}`
     container.appendChild(img)
 }
 
 
 function getRandomSize(){
-    return `${getRandomNr()}x${getRandomNr()}`
+    return `${getRandomNr()}`
 }
 
 function getRandomNr(){
-    return Math.floor(Math.random() * 10) + 300
+    return Math.floor(Math.random() * 10) + 100
 }
